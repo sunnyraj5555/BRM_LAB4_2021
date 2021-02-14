948f4761739b877943ac9b646bbd3166c7a88922
sunnyraj5555 <headheal2206@gmail.com>
2021-02-14 21:40:25 +0530

    Updating lab4

---
+++ src/main/java/Greeting.java
@@ -0,0 +1,11 @@
+
+public class Greeting {
+
+	public static void main(String[] args) {
+		System.out.println("Hello GitHub!");
+		System.out.println("Hello Maven!");
+		System.out.println("Hello jenkin!");
+		System.out.println("Hello World!");
+	}
+
+}
